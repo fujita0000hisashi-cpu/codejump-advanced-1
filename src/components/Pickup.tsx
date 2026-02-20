38767cdf414f5e15610bd857fe0dddd7bea6953c
fujita <fujita0000hisashi@gmail.com>
2026-02-20 13:58:05 +0900

    feat: style pickup cards

--- src/components/Pickup.tsx
+++ src/components/Pickup.tsx
@@ -4,16 +4,22 @@ export function Pickup() {
       <div className="container">
         <div className="pickup-grid">
           <article className="pickup-card">
-            <div className="pickup-thumb"></div>
-            <h2 className="pickup-title">Pickup 1</h2>
+            <a href="#" className="pickup-link">
+              <img className="pickup-thumb" src="/img/pickup1.jpg" alt="Pickup 1"/>
+              <h2 className="pickup-title">Pickup Title 1</h2>
+            </a>
           </article>
           <article className="pickup-card">
-            <div className="pickup-thumb"></div>
-            <h2 className="pickup-title">Pickup 2</h2>
+            <a href="#" className="pickup-link">
+              <img className="pickup-thumb" src="/img/pickup2.jpg" alt="Pickup 2"/>
+              <h2 className="pickup-title">Pickup Title 2</h2>
+            </a>
           </article>
           <article className="pickup-card">
-            <div className="pickup-thumb"></div>
-            <h2 className="pickup-title">Pickup 3</h2>
+            <a href="#" className="pickup-link">
+              <img className="pickup-thumb" src="/img/pickup3.jpg" alt="Pickup 3"/>
+              <h2 className="pickup-title">Pickup Title 3</h2>
+            </a>
           </article>
         </div>
       </div>
