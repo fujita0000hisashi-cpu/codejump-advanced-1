fd6f6e66c6c7437a7c2be614a8a8a9acc821a87c
fujita <fujita0000hisashi@gmail.com>
2026-02-20 14:26:23 +0900

    feat: add read more link

--- src/components/Pickup.tsx
+++ src/components/Pickup.tsx
@@ -4,22 +4,19 @@ export function Pickup() {
       <div className="container">
         <div className="pickup-grid">
           <article className="pickup-card">
-            <a href="#" className="pickup-link">
-              <img className="pickup-thumb" src="/img/pickup1.jpg" alt="Pickup 1"/>
-              <h2 className="pickup-title">Pickup Title 1</h2>
-            </a>
+            <img className="pickup-thumb" src="/img/pickup1.jpg" alt="Pickup 1"/>
+            <h2 className="pickup-title">Pickup Title 1</h2>
+            <a href="#" className="pickup-more">READ MORE</a>
           </article>
           <article className="pickup-card">
-            <a href="#" className="pickup-link">
-              <img className="pickup-thumb" src="/img/pickup2.jpg" alt="Pickup 2"/>
-              <h2 className="pickup-title">Pickup Title 2</h2>
-            </a>
+            <img className="pickup-thumb" src="/img/pickup2.jpg" alt="Pickup 2"/>
+            <h2 className="pickup-title">Pickup Title 2</h2>
+            <a href="#" className="pickup-more">READ MORE</a>
           </article>
           <article className="pickup-card">
-            <a href="#" className="pickup-link">
-              <img className="pickup-thumb" src="/img/pickup3.jpg" alt="Pickup 3"/>
-              <h2 className="pickup-title">Pickup Title 3</h2>
-            </a>
+            <img className="pickup-thumb" src="/img/pickup3.jpg" alt="Pickup 3"/>
+            <h2 className="pickup-title">Pickup Title 3</h2>
+            <a href="#" className="pickup-more">READ MORE</a>
           </article>
         </div>
       </div>
