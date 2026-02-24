37eba4bbac751578403cf65e9f3e51cdfb960cc7
fujita0000hisashi-cpu <fujita0000hisashi@gmail.com>
2026-02-24 11:01:22 +0900

    feat: add read more link to pickup section (#2)
    * feat: style pickup cards
    
    * feat: add read more link
    
    * fix: correct container class typo affecting layout width

--- src/components/Pickup.tsx
+++ src/components/Pickup.tsx
@@ -4,16 +4,19 @@ export function Pickup() {
       <div className="container">
         <div className="pickup-grid">
           <article className="pickup-card">
-            <div className="pickup-thumb"></div>
-            <h2 className="pickup-title">Pickup 1</h2>
+            <img className="pickup-thumb" src="/img/pickup1.jpg" alt="Pickup 1"/>
+            <h2 className="pickup-title">Pickup Title 1</h2>
+            <a href="#" className="pickup-more">READ MORE</a>
           </article>
           <article className="pickup-card">
-            <div className="pickup-thumb"></div>
-            <h2 className="pickup-title">Pickup 2</h2>
+            <img className="pickup-thumb" src="/img/pickup2.jpg" alt="Pickup 2"/>
+            <h2 className="pickup-title">Pickup Title 2</h2>
+            <a href="#" className="pickup-more">READ MORE</a>
           </article>
           <article className="pickup-card">
-            <div className="pickup-thumb"></div>
-            <h2 className="pickup-title">Pickup 3</h2>
+            <img className="pickup-thumb" src="/img/pickup3.jpg" alt="Pickup 3"/>
+            <h2 className="pickup-title">Pickup Title 3</h2>
+            <a href="#" className="pickup-more">READ MORE</a>
           </article>
         </div>
       </div>
