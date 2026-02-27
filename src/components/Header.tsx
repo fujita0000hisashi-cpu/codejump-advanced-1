e65a91139acb2530fd5e5cced237d61b2e62d648
fujita <fujita0000hisashi@gmail.com>
2026-02-27 12:57:05 +0900

    feat: update header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,25 @@
 export function Header() {
   return(
     <header className="header">
-      
-        <div className="header-inner">
-          <h1 className="header-logo">
-            <a className="header-logo-link" href="#">
-              <img className="header-logo-img" src="/img/logo.svg" alt="Logo" />
-            </a>
-          </h1>
+          <div className="header-top">
+            <h1 className="header-logo">
+              <a className="header-logo-link" href="#">
+                <img className="header-logo-img" src="/img/logo.svg" alt="Logo" />
+              </a>
+            </h1>
+          </div>
+          
           <nav className="header-nav" aria-label="Header navigation">
-            <ul className="header-nav-list">
-              <li className="header-nav-item"><a className="header-nav-link">NEW</a></li>
-              <li className="header-nav-item"><a className="header-nav-link">COLUMN</a></li>
-              <li className="header-nav-item"><a className="header-nav-link">SERIES</a></li>
-              <li className="header-nav-item"><a className="header-nav-link">Q&amp;A</a></li>
-              <li className="header-nav-item"><a className="header-nav-link">CONTACT</a></li>
-            </ul>
+            <div className="header-nav-inner">
+              <ul className="header-nav-list">
+                <li className="header-nav-item"><a className="header-nav-link">NEW</a></li>
+                <li className="header-nav-item"><a className="header-nav-link">COLUMN</a></li>
+                <li className="header-nav-item"><a className="header-nav-link">SERIES</a></li>
+                <li className="header-nav-item"><a className="header-nav-link">Q&amp;A</a></li>
+                <li className="header-nav-item"><a className="header-nav-link">CONTACT</a></li>
+              </ul>
+            </div>
           </nav>
-        </div>
-
     </header>
   );
 }
